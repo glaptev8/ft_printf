3f48599e745039487bccd12b6f29df593b72df98
gleblaptev <gleb.laptev.00@bk.ru>
2019-10-13 20:01:56 +0300

    разделение работы с андреем

--- src/display_d.c
+++ src/display_d.c
@@ -60,17 +60,14 @@ int		get_number_len(intmax_t n, t_printf list)
 	return (i);
 }
 
-void		display_d(t_printf *list)
+int		initialze_display_d(t_printf *list, intmax_t *n, int *len_num)
 {
-	intmax_t n;
-	int		len;
-	int		i;
-	int		len_num;
+	int len;
 
-	n = get_number_for_d(list);
-	list->number = n;
-	len = get_number_len(n, *list);
-	len_num = len;
+	*n = get_number_for_d(list);
+	list->number = *n;
+	len = get_number_len(*n, *list);
+	*len_num = len;
 	if (list->precision == 1 || list->minus == 1)
 	{
 		if (list->zero == 1)
@@ -83,8 +80,37 @@ void		display_d(t_printf *list)
 	if (list->minus == 1)
 		list->width = 0;
 	len += (list->precision_space > len ? list->precision_space - len : 0);
-	if ((list->number < 0 || list->plus == 1) && (list->precision_space > len_num - 1))
+	if ((list->number < 0 || list->plus == 1) && (list->precision_space > *len_num - 1))
 		len++;
+	return (len);
+}
+
+void		display_d(t_printf *list)
+{
+	intmax_t n;
+	int		len;
+	int		i;
+	int		len_num;
+
+	len = initialze_display_d(list, &n, &len_num);
+//	n = get_number_for_d(list);
+//	list->number = n;
+//	len = get_number_len(n, *list);
+//	len_num = len;
+//	if (list->precision == 1 || list->minus == 1)
+//	{
+//		if (list->zero == 1)
+//		{
+//			list->width_space = list->zero_space;
+//			list->width = 1;
+//		}
+//		list->zero = 0;
+//	}
+//	if (list->minus == 1)
+//		list->width = 0;
+//	len += (list->precision_space > len ? list->precision_space - len : 0);
+//	if ((list->number < 0 || list->plus == 1) && (list->precision_space > len_num - 1))
+//		len++;
 	if (list->space == 1 && list->plus != 1 && list->number >= 0)
 	{
 		len++;
